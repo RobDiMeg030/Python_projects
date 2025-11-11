5d3459ed58cca072c2f718028c30279cea4a1244
RobDiMeg030 <r.dimeglio@outlook.de>
2025-11-11 12:10:22 +0100

    Version 1.25 - better catgeory

--- rugby_project/rugby_project_gui.py
+++ rugby_project/rugby_project_gui.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import tkinter as tk
 from tkinter import ttk, messagebox
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from tkcalendar import DateEntry
 
 
 def wrap_by_words(text, words_per_line=4):
@@ -15,6 +16,19 @@ def wrap_by_words(text, words_per_line=4):
     lines = [' '.join(words[i:i+words_per_line]) for i in range(0, len(words), words_per_line)]
     return '\n'.join(lines)
 
+def new_cathegory(competition):
+    if pd.isnull(competition):
+        return "Test Match"
+    turnier=str(competition).lower()
+    if "nations" in turnier:
+        return "Six Nations"
+    elif "Rugby Championship" in turnier:
+        return "Rugby Championship"
+    elif "world cup" in turnier:
+        return "Rugby World Cup"
+    else:
+        return "Test Match"
+
 # Download latest version
 path = kagglehub.dataset_download("lylebegbie/international-rugby-union-results-from-18712022")
 #get the downlaoded data
@@ -22,6 +36,8 @@ print("Path to dataset files:", path)
 new_path=path+"\\results.csv"
 #create dataframe
 rugby=pd.read_csv(new_path)
+rugby['Match Category'] = rugby['competition'].apply(new_cathegory)
+rugby=rugby.drop(columns=['competition','neutral','country','world_cup'])
 teams = sorted(set(rugby['home_team']).union(set(rugby['away_team'])))
 
 # GUI-Funktion: Match-Stats für ausgewähltes Team
@@ -35,6 +51,19 @@ team_dropdown = ttk.Combobox(root, textvariable=team_var, values=teams, state="r
 team_dropdown.set("Team auswählen")
 team_dropdown.grid(row=0, column=0, padx=10, pady=10)
 
+# Startdatum
+start_label = tk.Label(root, text="Von:")
+start_label.grid(row=0, column=1, padx=5, pady=10)
+start_date = DateEntry(root, width=12, background='darkblue', foreground='white', borderwidth=2, date_pattern='yyyy-mm-dd')
+start_date.grid(row=0, column=2, padx=5, pady=10)
+
+# Enddatum
+end_label = tk.Label(root, text="Bis:")
+end_label.grid(row=0, column=3, padx=5, pady=10)
+end_date = DateEntry(root, width=12, background='darkblue', foreground='white', borderwidth=2, date_pattern='yyyy-mm-dd')
+end_date.grid(row=0, column=4, padx=5, pady=10)
+
+
 # Ergebnis-Label
 result_label = tk.Label(root, text="", justify="left", font=("Arial", 10), anchor="w")
 result_label.grid(row=1, column=0, padx=10, pady=10, sticky="w")
@@ -45,7 +74,21 @@ canvas_frame.grid(row=2, column=0, padx=10, pady=10)
 
 def update_report(*args):
     team = team_var.get()
-    rugby_team = rugby[(rugby['home_team'] == team) | (rugby['away_team'] == team)]
+
+    start = pd.to_datetime(start_date.get_date())
+    end = pd.to_datetime(end_date.get_date())
+
+    rugby_team = rugby[
+        ((rugby['home_team'] == team) | (rugby['away_team'] == team)) &
+        (pd.to_datetime(rugby['date']) >= start) &
+        (pd.to_datetime(rugby['date']) <= end)
+        ]
+
+    if rugby_team.empty:
+        result_label.config(text=f"Keine Spiele für {team} im gewählten Zeitraum ({start.date()} bis {end.date()}).")
+        for widget in canvas_frame.winfo_children():
+            widget.destroy()
+        return
 
     # Statistiken berechnen
     wins = losses = draws = points = 0
@@ -78,12 +121,12 @@ def update_report(*args):
     # Letzte 5 Spiele als Tabelle
     df_last5 = rugby_team.tail(5)
 
-    fig, axs = plt.subplots(1, 2, figsize=(15, 10))
+    fig, axs = plt.subplots(2, 1, figsize=(15, 10))
     axs[0].axis('off')
     table = axs[0].table(cellText=df_last5.values, colLabels=df_last5.columns, loc='center')
     table.auto_set_font_size(False)
-    table.set_fontsize(8)
-    table.scale(1.2, 1.2)
+    table.set_fontsize(6)
+    table.scale(1.5, 1.2)
     axs[0].set_title("Letzte 5 Spiele")
 
     # Tortendiagramm der Gegner
