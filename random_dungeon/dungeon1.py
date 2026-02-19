420509333e3a56c83838a833b415df68d3af8d7d
RobDiMeg030 <r.dimeglio@outlook.de>
2026-02-19 11:46:58 +0100

    v 0.19 - Fight options ar implemimented - Blocking needs improvement

--- random_dungeon/dungeon1.py
+++ random_dungeon/dungeon1.py
@@ -1,5 +1,7 @@
 import random
 
+from sqlalchemy.sql.operators import truediv
+
 
 # room class
 
@@ -20,6 +22,107 @@ class Room:
 
         }
         return description[self.type]
+
+#player class
+class Player:
+    def __init__(self):
+        self.hp= 25
+        self.attack=random.randint(3,7)
+        self.defense=random.randint(2,5)
+        self.inventory=[]
+
+## npc class
+class Npc:
+    def __init__(self,type):
+        self.type = type
+
+        stats={
+            "enemy": {
+                "name": "Goblin",
+                "hp": 12,
+                "attack": random.randint(1, 4),
+                "defense": random.randint(0, 2)
+            },
+            "boss": {
+                "name": "Dämonenlord",
+                "hp": 30,
+                "attack": random.randint(5, 8),
+                "defense": random.randint(2, 4)
+            }
+        }
+        npc_stats = stats[type]
+
+        self.name = npc_stats["name"]
+        self.hp = npc_stats["hp"]
+        self.attack = npc_stats["attack"]
+        self.defense = npc_stats["defense"]
+
+
+# action class
+
+class Action:
+    def __init__(self,name,function):
+        self.name=name
+        self.function=function
+
+    def use_item(self,player):
+        print("soon...")
+    def block(self,player):
+        print("You brace yourself and block")
+
+        player.defense += 2
+        print("Your defense temporarily increased!")
+
+    def fight(player,npc):
+        print("Fight Started")
+        print(f"Your opponent {npc.name} - HP: {npc.hp} - Attack:{npc.attack} - Defense:{npc.defense}")
+        print(f"Your Stats - HP:{player.hp} - Attack:{player.attack} - Defense:{player.defense}")
+
+        while npc.hp > 0 and player.hp >0:
+    # giving options
+            choice=input("""What you are gonna do?
+                    F - Fight
+                    B - Block
+                    I - Item
+                    Your Choice: """)
+
+            if choice == "I":
+
+                Action.use_item(player)
+            elif choice == "B":
+                Action.block(player)
+            elif choice == "F":
+            #players turn
+
+                dmg_2_enm= max(0, player.attack- npc.defense)
+                npc.hp -= dmg_2_enm
+                print("You Attack....")
+                if dmg_2_enm>0:
+                    print(f"You attacked and dealt {dmg_2_enm} damage")
+                    print(f"Enemy's HP: {npc.hp}")
+                else:
+                    print("You missed!")
+
+                if npc.hp <= 0:
+                    print(f" {npc.name} is dead")
+                    return True
+
+            # enemy turn
+                dmg_2_play = max(0, npc.attack - player.defense)
+                player.hp -= dmg_2_play
+                print("Your Enemy Attack....")
+                if dmg_2_play > 0:
+                    print(f"Your enemy attacked and dealt {dmg_2_play} damage")
+                    print(f"Your HP: {player.hp}")
+                else:
+                    print("Your Enemy  missed!")
+
+                if player.hp <= 0:
+                    print(f" your dead")
+                    exit()
+                    return False
+
+
 
 
 # dungeon class
@@ -54,14 +157,23 @@ class Dungeon:
         rooms.append(Room("boss"))
         return rooms
 
+# test
 
 ### main game
 
 def main():
     dungeon=Dungeon(size=6)
+    player = Player()
     print("Welcome to Dungeon !!!")
     for i,room in enumerate(dungeon.rooms):
         print(f"{i}. {room.describe()}")
+        if room.type == "enemy":
+            npc = Npc("enemy")
+            Action.fight(player, npc)
+
+        elif room.type == "boss":
+            npc = Npc("boss")
+            Action.fight(player, npc)
 
         input("Press Enter to continue in the dungeon...")
 
